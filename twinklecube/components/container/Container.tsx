fc08dd4011287236fbb82a3f845d21653fbe566f
twinklecube <twinklecubeui@gmail.com>
2022-11-10 18:58:55 +0100

    added assets to dev server and build

--- twinklecube/components/container/Container.tsx
+++ twinklecube/components/container/Container.tsx
@@ -3,6 +3,7 @@ import {Routes, Route} from "react-router-dom";
 import ChipTwinklecube from "../../../src/components/chip/Chip.twinklecube";
 import IconTwinklecube from "../../../src/components/icon/Icon.twinklecube";
 import CardTwinklecube from "../../../src/components/card/Card.twinklecube";
+import {Chip} from "../../../dist/esm";
 // @ts-ignore
 import style from "./style.module.css"
 
@@ -10,9 +11,11 @@ const Container = () => {
     return (
         <div className={style["container-wrapper"]}>
             <Routes>
-                <Route path="/chip" element={<ChipTwinklecube />} />
+                {/*<Route path="/chip" element={<ChipTwinklecube />} />*/}
+                <Route path="/chip" element={<Chip>zdc</Chip>} />
                 <Route path="/icon" element={<IconTwinklecube />} />
                 <Route path="/card" element={<CardTwinklecube />} />
+
                 {/*more components come here*/}
             </Routes>
         </div>
