bba7eedb18e3c1c87b024707d7fa286c4d91073b
twinklecube <twinklecubeui@gmail.com>
2022-11-18 22:09:28 +0100

    added some more styling

--- twinklecube/components/header/Header.tsx
+++ twinklecube/components/header/Header.tsx
@@ -2,12 +2,13 @@ import * as React from 'react';
 // @ts-ignore
 import style from "./style.module.css";
 // @ts-ignore
-import logo from '../../logo.png'
+import logo from '../../logo.png';
+import {Link} from "react-router-dom";
 
 const Header = () => {
     return (
         <div className={style["header-wrapper"]}>
-            <img src={logo}/>
+            <Link to="/"><img src={logo}/></Link>
         </div>
     );
 };
