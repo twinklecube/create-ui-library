71b4dee5891472fd54747d259f1e211fd486130c
twinklecube <twinklecubeui@gmail.com>
2022-11-20 16:55:40 +0100

    testing all styles

--- src/components/chip/Chip.tsx
+++ src/components/chip/Chip.tsx
@@ -2,6 +2,12 @@ import React, {ReactNode} from 'react';
 import {ChipType} from "./types";
 import {ChipWrapper} from "./style";
 import {Icon} from "../icon/Icon";
+import style from './style.module.css';
+import lessStyle from './style.module.less';
+import scssStyle from './chip.module.scss'
+import './style.css'
+import './style.less'
+import './style.scss'
 
 interface Props {
     children: ReactNode;
@@ -17,9 +23,17 @@ export const Chip = ({
     active
 }: Props) => {
     return (
+        <>
             <ChipWrapper error={error} active={active} type={type}>
                 {children}
                 {type === ChipType.ICON && <Icon />}
             </ChipWrapper>
+            <div className="chip-css">style css</div>
+            <div className={style.chipModuleCss}>style module css</div>
+            <div className="chip-less">chip less</div>
+            <div className={lessStyle.chipModuleLess}>chip module less</div>
+            <div className="chip-scss">chip scss</div>
+            <div className={scssStyle.chipModuleScss}>chip module scss</div>
+        </>
     );
 };
